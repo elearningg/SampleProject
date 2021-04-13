3552ae790eb7e340902ebb4c64225ef56ff7a028
Supriya Anchan <anchansupriya7@gmail.com>
2021-04-14 00:05:49 +0530

    changes done

--- sample_project/src/sample_project/Sample_project.java
+++ sample_project/src/sample_project/Sample_project.java
@@ -3,7 +3,7 @@ package sample_project;
 public class Sample_project {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World ");
+		System.out.println("Hello World java");
 
 	}
 
